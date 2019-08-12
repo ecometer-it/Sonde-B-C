cdb1c6e46daa82095b9b1f42a06d5e9d256dce29
saudin17 <53856989+saudin17@users.noreply.github.com>
2019-08-12 15:45:21 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -13,8 +13,8 @@ __version__ = '1.0'
 
 source_suffix = '.rst'
 master_doc = 'index'
-project = 'CHANGE-THIS'
-copyright = '2016, CHANGE-THIS'
+project = 'Sonde B&C'
+copyright = '2019, Ecometer'
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
